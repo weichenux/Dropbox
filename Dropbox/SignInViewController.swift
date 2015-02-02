c2b15add222573397b486220e2381927068aa035
weichenux <chanwei@umich.edu>
2015-02-01 19:57:44 -0800

    Assignment Week2
    initial commit

---
+++ Dropbox/SignInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignInViewController.swift
+//  Dropbox
+//
+//  Created by Wei Chen on 2/1/15.
+//  Copyright (c) 2015 Wei Chen. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var sbackButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onButton(sender: AnyObject) {
+        navigationController!.popViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
